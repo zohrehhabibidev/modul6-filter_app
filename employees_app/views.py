be373f81e196a7ca476969c00996a6b354c0ff5b
Zohreh Habibi <habibi.z@outlook.de>
2026-09-21 09:12:50 +0200

    feat: filter employees earning over 3000

--- employees_app/views.py
+++ employees_app/views.py
@@ -7,9 +7,11 @@ from datetime import date
 def employee_overview(request):
 
     employees = Employee.objects.all()
+    high_earners = Employee.objects.filter(salary__gt=3000)
 
     context = {
         "employees": employees,
+        "high_earners": high_earners,
     }
 
     return render(request, "employee_list.html", context)
