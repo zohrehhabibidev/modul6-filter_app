9ef2f2de2d4d238bcc67152c687e6e82cb9bf4c6
Zohreh Habibi <habibi.z@outlook.de>
2026-09-21 09:15:58 +0200

    feat: calculate average sales salary

--- employees_app/views.py
+++ employees_app/views.py
@@ -9,11 +9,17 @@ def employee_overview(request):
     employees = Employee.objects.all()
     high_earners = Employee.objects.filter(salary__gt=3000)
     employees_5000 = Employee.objects.filter(salary__gte=5000).count()
+    sales_average = Employee.objects.filter(
+        department__name="Sales"
+    ).aggregate(
+        avg_salary=Avg("salary")
+    )["avg_salary"]
 
     context = {
         "employees": employees,
         "high_earners": high_earners,
         "employees_5000": employees_5000,
+        "sales_average": sales_average,
     }
 
     return render(request, "employee_list.html", context)
