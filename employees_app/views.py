f33d751cbb27025402c17db307c9c7e694ea1fe7
Zohreh Habibi <habibi.z@outlook.de>
2026-09-21 09:14:36 +0200

    feat: count employees earning at least 5000

--- employees_app/views.py
+++ employees_app/views.py
@@ -8,10 +8,12 @@ def employee_overview(request):
 
     employees = Employee.objects.all()
     high_earners = Employee.objects.filter(salary__gt=3000)
+    employees_5000 = Employee.objects.filter(salary__gte=5000).count()
 
     context = {
         "employees": employees,
         "high_earners": high_earners,
+        "employees_5000": employees_5000,
     }
 
     return render(request, "employee_list.html", context)
